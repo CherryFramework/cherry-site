5c7b6962c125df21d3967fe43f18e16301718e66
cheh <chehovskiy.dima@gmail.com>
2017-05-31 11:27:42 +0300

    UPD: order popup styles

--- template-parts/order-form.php
+++ template-parts/order-form.php
@@ -9,8 +9,8 @@
 <section id="cherry-orderpopup" class="cherry-order-popup">
 	<div class="cherry-order-popup__overlay"></div>
 	<div class="cherry-order-popup__container container-fill-color-type">
+		<span id="cherry-orderpopup-close" class="cherry-order-popup__close">&#215;</span>
 		<div class="cherry-order-popup__container-inner">
-			<span id="cherry-orderpopup-close" class="cherry-order-popup__close">&#215;</span>
 			<h1 class="cherry-order-popup__title"><?php echo esc_html__( 'Order Details', 'cherry' ); ?></h1>
 			<div class="cherry-order-popup__content">
 				<?php echo do_shortcode( '[contact-form-7 id="95" title="Order Form"]' ); ?>
